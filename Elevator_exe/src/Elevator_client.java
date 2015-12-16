2923f7d2791cdb4c1307275888cd8bfc6e6a931a
suraj <surajkorgaonkar30@gmail.com>
2015-12-16 15:04:16 +0530

    succcessful

---
+++ Elevator_exe/src/Elevator_client.java
@@ -0,0 +1,17 @@
+//main class_1438
+import java.util.ArrayList;
+
+public class Elevator_client {
+
+	public static void main(String[] args) {
+		Elevator elevator = new Elevator(5);
+		Floorpanel floorpanel = new Floorpanel();
+	    floorpanel.attachment(elevator);
+	 ///Elevatorpanel elevatorpanel = new Elevatorpanel();
+	    Door door= new Door();
+	    
+	    
+	    
+	}
+
+}
