6f7f17530363b334b3cd6d001dbee0386a330ff6
suraj <surajkorgaonkar30@gmail.com>
2016-01-06 11:59:57 +0530

    flipcart

---
+++ item_exam1/src/item_exam1/Item.java
@@ -0,0 +1,19 @@
+package item_exam1;
+
+public class Item {
+	
+
+	public Object getPrice() {
+		// TODO Auto-generated method stub
+		int Bag=1000;
+		return Bag;
+	}
+	
+	public Object getdisp() {
+		String mobile1;
+		// TODO Auto-generated method stub
+		mobile1= "Mobile";
+			return mobile1;
+	}
+
+}
